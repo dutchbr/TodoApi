c6ba74fde5ca33f743034d720045ef939a140590
Anezio Nunes Silva <dutchbr@gmail.com>
2020-04-17 06:40:26 -0300

    Adicionar arquivos de projeto.

---
+++ Models/TodoContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Models
+{
+    public class TodoContext:DbContext
+    {
+
+        protected TodoContext(DbContextOptions options) : base(options) { }
+
+        public TodoContext(DbContextOptions<TodoContext> options) : this((DbContextOptions)options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+
+
+    }
+}
