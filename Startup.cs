fec25f2aa44e5b1e5a1cc7d523c6916ef66dfb8f
Anezio Nunes Silva <dutchbr@gmail.com>
2020-04-17 06:42:48 -0300

    Primeiro Commit

--- Startup.cs
+++ Startup.cs
@@ -34,6 +34,7 @@ namespace TodoApi
             services.AddControllers();
 
 
+
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
